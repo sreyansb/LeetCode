15cfb02f928d368bb74cc4ba8edbbaf8cd4f3cb3
Sreyans Bothra <sreyansrocks@gmail.com>
2020-07-23 15:44:32 +0530

    23

---
+++ JULY2020/23.py
@@ -0,0 +1,42 @@
+#3rd attempt->Clues
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        a=0
+        for i in nums:
+            a^=i
+        j=a&-a
+        ans=0
+        for i in nums:
+            if j&i:
+                ans^=i
+        return [ans,a^ans]
+
+
+"""
+#2nd attempt->faster
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        a=0
+        for i in nums:
+            a^=i
+        for j in bin
+"""            
+        
+
+"""
+#1st attempt->very slow
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        di={}
+        for i in nums:
+            if i not in di:
+                di[i]=0
+            di[i]+=1
+        l=[]
+        for i in di:
+            if di[i]==1:
+                l.append(i)
+        return l
+"""
+
+        

e0973d0c1d4140f8c84b22489e4c8015dda185a3
Sreyans Bothra <sreyansrocks@gmail.com>
2020-07-26 18:31:57 +0530

    26

---
+++ JULY2020/26.py
@@ -0,0 +1,13 @@
+
+"""
+#first attempt
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while(num>9):
+            s=0
+            while(num):
+                s+=num%10
+                num//=10
+            num=s
+        return num
+"""

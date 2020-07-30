6bd5c41fc9d1063f25215c12e59aaa8357780d1f
Sreyans Bothra <sreyansrocks@gmail.com>
2020-07-30 20:48:52 +0530

    30

---
+++ JULY2020/30.py
@@ -0,0 +1,55 @@
+#ACCEPTED:
+def wordie(s,worddict,memo,answer):
+    #print(s,memo)
+    if s=="":
+        return ['']  
+    if s in memo:
+        return memo[s]
+    res=[]
+    for i in range(1,len(s)+1):
+        if s[:i] in worddict:
+            for j in wordie(s[i:],worddict,memo,answer):
+               res.append((s[:i] + " " + j).strip())       
+    memo[s]=res
+    return memo[s]
+        
+class Solution:
+    def wordBreak(self, s, wordDict):
+        wordDict=set(wordDict)
+        answer=[]
+        sentence=[]
+        return wordie(s,wordDict,{},answer)
+#Not Accepted
+"""
+def wordie(s,worddict,memo,answers,sentence):
+    #print(s,sentence)
+    if s=="":
+        return True  
+    if s in worddict:
+        sentence.append(s)
+    for i in range(1,len(s)+1):
+        if s[:i] in worddict:
+            sentence.append(s[:i])
+            if i==len(s):
+                answers.add(" ".join(sentence))
+                sentence.pop()
+            k=wordie(s[i:],worddict,memo,answers,sentence)
+            sentence.pop()           
+    return False
+        
+class Solution:
+    def wordBreak(self, s, wordDict):
+        wordDict=set(wordDict)
+        answers=set()
+        sentence=[]
+        wordie(s,wordDict,{},answers,sentence)
+        if sentence:
+            answers.add(" ".join(sentence))
+        return list(answers)
+"""
+obj=Solution()
+print(obj.wordBreak("aaaaaaa",["aaaa","aa","a"]))
+#print(obj.wordBreak("pineapplepenapple",\
+#                    ["apple", "pen", "applepen", "pine", "pineapple"]))
+#print(obj.wordBreak("catsanddog",["cat", "cats", "and", "sand", "dog"]))
+#print(obj.wordBreak("catsandog",["cat", "cats", "and", "sand", "dog"]))

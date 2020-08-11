a70f40d4e3bbe33cd51b50a4e4161ad0b69ad41a
Sreyans Bothra <sreyansrocks@gmail.com>
2020-08-11 16:02:01 +0530

    Some more

--- RANDOM/LEETCODEBinaryTreePruning.py
+++ RANDOM/LEETCODEBinaryTreePruning.py
@@ -4,6 +4,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#fastest attempt
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def helper(root):
+    if not(root):
+        return False
+    if root.val:
+        root.left=root.left if helper(root.left) else None
+        root.right=root.right if helper(root.right) else None
+        return True
+    else:
+        root.left=root.left if helper(root.left) else None
+        root.right=root.right if helper(root.right) else None
+        if root.left==None and root.right==None:
+            return False
+        else:
+            return True
+        
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if helper(root):
+            return root
+        else:
+            return None
+        
+            
+                
+        
+"""
 def helper(root):
     if not(root):
         return False
@@ -25,7 +58,7 @@ class Solution:
             return root
         else:
             return None
-        
+"""     
             
                 
         

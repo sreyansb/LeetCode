44e678d87a6149becf559d510c75ee0d455286fa
Sreyans Bothra <sreyansrocks@gmail.com>
2020-12-03 21:33:58 +0530

    3

---
+++ DEC2020/3.py
@@ -0,0 +1,45 @@
+#attempt2: 60%
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        l=[]
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                l.append(root)
+                inorder(root.right)
+        inorder(root)
+        for i in range(len(l)-1):
+            l[i].right=l[i+1]
+            l[i].left=None
+        l[-1].left=None
+        return l[0]
+
+#attempt1:WA
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        l=[]
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                l.append(root)
+                inorder(root.right)
+        inorder(root)
+        for i in range(len(l)-1):
+            l[i].right=l[i+1]
+            l[i].left=None
+        return l[0]
+                
+'''

8b9031f5f785ab321258c073d138fb7375d8c9ef
Sreyans Bothra <sreyansrocks@gmail.com>
2021-01-23 14:12:34 +0530

    23

---
+++ JAN2021/23.py
@@ -0,0 +1,19 @@
+#attempt1:
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m=len(mat)
+        n=len(mat[0])
+        di={}
+        for i in range(m):
+            for j in range(n):
+                if i-j not in di:
+                    di[i-j]=[[],0]
+                di[i-j][0].append(mat[i][j])
+        for i in di:
+            di[i][0].sort()
+        for i in range(m):
+            for j in range(n):
+                mat[i][j]=di[i-j][0][di[i-j][1]]
+                di[i-j][1]+=1
+        return mat
+        

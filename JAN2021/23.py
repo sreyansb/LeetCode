1051d0eb2e6c43af440553550f81a61218570caa
Sreyans Bothra <sreyansrocks@gmail.com>
2021-01-23 17:32:47 +0530

    23

--- JAN2021/23.py
+++ JAN2021/23.py
@@ -1,4 +1,22 @@
+#attempt2:
+from heapq import heappush,heappop
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m=len(mat)
+        n=len(mat[0])
+        di={}
+        for i in range(m):
+            for j in range(n):
+                if i-j not in di:
+                    di[i-j]=[]
+                heappush(di[i-j],mat[i][j])
+        for i in range(m):
+            for j in range(n):
+                mat[i][j]=heappop(di[i-j])
+        return mat
+
 #attempt1:
+'''
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         m=len(mat)
@@ -16,4 +34,4 @@ class Solution:
                 mat[i][j]=di[i-j][0][di[i-j][1]]
                 di[i-j][1]+=1
         return mat
-        
+'''

ef3946259689c4718b610f663b93c4952b00f8b9
Sreyans <sreyansrocks@gmail.com>
2021-06-07 12:42:36 +0530

    7

---
+++ JUNE2021/7.py
@@ -0,0 +1,14 @@
+#aattempt1:
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n=len(cost)
+        dp={}
+        def findcost(index):
+            if index>=n:
+                return 0
+            if index not in dp:
+                dp[index]=cost[index]+min(findcost(index+1),findcost(index+2))
+            return dp[index]
+        return min(findcost(0),findcost(1))
+            
+            

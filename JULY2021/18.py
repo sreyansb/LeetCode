7bf7a305903dd5a88b1f8aa305d24ec8726bb902
Sreyans <sreyansrocks@gmail.com>
2021-07-18 15:14:34 +0530

    18:Very important

--- JULY2021/18.py
+++ JULY2021/18.py
@@ -1,3 +1,47 @@
+#attempt4: Recurive solution without extra explicit array space 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        count=0
+        node=head
+        ans=head
+        while(node):
+            count+=1
+            if count==k:
+                ans=node
+            node=node.next
+        if count<k:
+            return ans
+        def reverse(first,last):
+            prev=last
+            while(first!=last):
+                temp=first.next
+                first.next=prev
+                prev=first
+                first=temp
+            return prev
+        def recurse(head):
+            #print(head)
+            headc=head
+            i=0
+            parent=None
+            while(headc and i<k):
+                parent=headc
+                headc=headc.next
+                i+=1
+            if i<k:
+                return head
+            headc=recurse(headc)
+            parent.next=headc
+            head=reverse(head,headc)
+            return head
+        return recurse(head)
+
 #attempt3:
 # Definition for singly-linked list.
 # class ListNode:

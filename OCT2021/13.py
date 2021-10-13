97867c03ab50e87117bc8362e60609be5ad83c0c
Sreyans <sreyansrocks@gmail.com>
2021-10-13 11:22:50 +0530

    13

--- OCT2021/13.py
+++ OCT2021/13.py
@@ -2,11 +2,12 @@
 # start of the array
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         inorder = sorted(preorder)

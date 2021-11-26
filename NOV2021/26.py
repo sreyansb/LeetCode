19824ee94e5386d8ccaa20487c8b99c29bb50cc1
Sreyans <sreyansrocks@gmail.com>
2021-11-26 14:42:15 +0530

    26

---
+++ NOV2021/26.py
@@ -0,0 +1,52 @@
+#attempt2:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        start = 0
+        end = n-1
+        ans = -1
+        while(start<=end):
+            mid = (start+end)//2
+            if nums[mid]<target:
+                if mid+1<n and nums[mid+1]>=target:
+                    return mid+1
+                start = mid+1
+            elif nums[mid]>target:
+                end = mid-1
+            else:
+                ans = mid
+                end = mid-1
+        if ans == -1:
+            if target<nums[0]:
+                return 0
+            elif target>nums[-1]:
+                return n
+        return ans
+        
+
+#attempt1:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        start = 0
+        end = n-1
+        ans = -1
+        while(start<=end):
+            mid = (start+end)//2
+            if nums[mid]<target:
+                if mid+1<n and nums[mid+1]>target:
+                    return mid+1
+                start = mid+1
+            elif nums[mid]>target:
+                end = mid-1
+            else:
+                ans = mid
+                end = mid-1
+        if ans == -1:
+            if target<nums[0]:
+                return 0
+            elif target>nums[-1]:
+                return n
+        return ans
+        
+        

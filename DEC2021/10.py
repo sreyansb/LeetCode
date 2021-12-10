ec1deb63022652228ccd1669c4eb8d45f29c5379
Sreyans <sreyansrocks@gmail.com>
2021-12-10 22:38:57 +0530

    WTF

--- DEC2021/10.py
+++ DEC2021/10.py
@@ -7,7 +7,7 @@ class Solution:
         f[2] = 2
         f[3] = 5
         for i in range(4,n+1):
-            f[i] = 2*f[i-1] + f[i-3]
+            f[i] = 2*f[i-1] + f[i-3]#asaks
         return f[n]%1000000007
             
                 

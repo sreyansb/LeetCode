866d25e312f944dad1b053ce3ce3b3392a8b88c0
Sreyans <sreyansrocks@gmail.com>
2022-01-17 19:10:59 +0530

    17

---
+++ JAN2022/16.py
@@ -0,0 +1,20 @@
+#attempt1:
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        n = len(seats)
+        dist = [float('inf') for i in range(n)]
+        
+        for i in range(n):
+            if seats[i]:
+                dist[i] = 0
+            else:
+                dist[i] = dist[i-1] + 1 if i-1>=0 else float('inf')
+        
+        for i in range(n-2,-1,-1):
+            if seats[i]:
+                dist[i] = 0
+            else:
+                dist[i] = min(dist[i],dist[i+1] + 1)
+        
+        return max(dist)
+        

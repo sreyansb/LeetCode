a8fe99046790526502edb7887ba47811dd5f6f62
Sreyans <sreyansrocks@gmail.com>
2022-02-14 17:32:22 +0530

    14

---
+++ FEB2022/13.py
@@ -0,0 +1,11 @@
+#attempt1:
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        final = [[]]
+        for i in nums:
+            new = []
+            for j in final:
+                new.append(j+[i])
+            final.extend(new)
+        return final
+        

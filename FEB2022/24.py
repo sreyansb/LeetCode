7e73c751497fa853c37bcdb9f338ebc17311a6ec
Sreyans <sreyansrocks@gmail.com>
2022-02-24 22:54:09 +0530

    24

--- FEB2022/24.py
+++ FEB2022/24.py
@@ -1,59 +1,19 @@
-#attempt2: AC
+#attempt1:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def compareVersion(self, v1: str, v2: str) -> int:
-        ans = 0
-        v1 = v1.split(".")
-        v2 = v2.split(".")
-        v1len = len(v1)
-        v2len = len(v2)
-        reverse = 1
-        if v1len<v2len:
-            v1,v2 = v2,v1
-            v1len,v2len = v2len,v1len
-            reverse = -1
-        for index in range(v1len):
-            ele1 = int(v1[index])
-            try:
-                ele2 = int(v2[index])
-            except:
-                ele2 = 0
-            if ele1>ele2:
-                ans = 1
-                break
-            elif ele2>ele1:
-                ans = -1
-                break
-        return ans*reverse
-
-#attempt1: WA because didn't take into consideration reversal of v1 and v2
-'''
-class Solution:
-    def compareVersion(self, v1: str, v2: str) -> int:
-        ans = 0
-        v1 = v1.split(".")
-        v2 = v2.split(".")
-        v1len = len(v1)
-        v2len = len(v2)
-        if v1len<v2len:
-            v1,v2 = v2,v1
-            v1len,v2len = v2len,v1len
-        for index in range(v1len):
-            ele1 = int(v1[index])
-            try:
-                ele2 = int(v2[index])
-            except:
-                ele2 = 0
-            if ele1>ele2:
-                ans = 1
-                break
-            elif ele2>ele1:
-                ans = -1
-                break
-        return ans
-            
-                
-        
-'''
-            
-                
-        
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        eles = []
+        while(head):
+            eles.append(head.val)
+            head = head.next
+        eles.sort()
+        newone = ListNode()
+        newc = newone
+        for i in eles:
+            newone.next = ListNode(i)
+            newone = newone.next
+        return newc.next

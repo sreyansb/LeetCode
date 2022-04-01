d0315393db74d9e2021b538a3d7e6f747bc1f417
Sreyans <sreyansrocks@gmail.com>
2022-04-01 14:34:55 +0530

    1st April 2022

---
+++ APR2022/1.py
@@ -0,0 +1,10 @@
+#attempt1:
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        low = 0
+        high = len(s)-1
+        while(low<high):
+            s[low],s[high] = s[high],s[low]
+            low += 1
+            high -= 1
+        

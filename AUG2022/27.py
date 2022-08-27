72d1185db0669d5233156316e4954709c3f2b988
Sreyans <sreyansrocks@gmail.com>
2022-08-27 17:09:36 +0530

    27

--- AUG2022/27.py
+++ AUG2022/27.py
@@ -1,16 +1,23 @@
 #attempt1:
+from sortedcontainers import SortedList
 class Solution:
-    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        di = {}
-        m,n = len(mat),len(mat[0])
-        for row in range(m):
-            for col in range(n):
-                if row-col not in di:
-                    di[row-col] = []
-                di[row-col].append(mat[row][col])
-        for diagdiff in di:
-            di[diagdiff].sort(reverse=True)
-        for row in range(m):
-            for col in range(n):
-                mat[row][col] = di[row-col].pop()
-        return mat
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        rowlen = len(matrix)
+        collen = len(matrix[0])
+        for row in range(rowlen):
+            for col in range(1,collen):
+                matrix[row][col] += matrix[row][col-1]
+        maxans = -float('inf')
+        for col1 in range(collen):
+            for col2 in range(col1,collen):
+                sumswehaveseen = SortedList([0])
+                lenoflist = 1
+                currentsum = 0
+                for row in range(rowlen):
+                    currentsum += matrix[row][col2] - (matrix[row][col1-1] if col1>0 else 0)
+                    posinlist = sumswehaveseen.bisect_left(currentsum - k)
+                    if posinlist != lenoflist:
+                        maxans = max(maxans,currentsum-sumswehaveseen[posinlist])
+                    sumswehaveseen.add(currentsum)
+                    lenoflist += 1
+        return maxans

24482840840e6c67a785a241b23b366d0e2a3353
Sreyans <sreyansrocks@gmail.com>
2023-01-05 20:15:23 +0530

    5

---
+++ JAN2023/5.py
@@ -0,0 +1,16 @@
+#attempt1: Copied previous submission
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        ans = 1
+        points.sort(key=lambda x:(x[0],x[1]))
+        xs,xe = points[0]
+        for x1,x2 in points[1:]:
+            if x1>xe:
+                ans += 1
+                xs,xe = x1,x2
+            else:
+                xe = min(xe,x2)
+        return ans
+            
+            
+        

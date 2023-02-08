7a41aee9cbf6cf2f1c6884c807cb2c7a854afa4a
Sreyans <sreyansrocks@gmail.com>
2023-02-08 17:32:48 +0530

    8

---
+++ FEB2023/8.py
@@ -0,0 +1,14 @@
+#attempt1: JUST  COPIED
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n=len(nums)
+        mins=[float('inf')]*n
+        mins[-1]=0
+        for i in range(n-2,-1,-1):
+            if i+nums[i]>=n-1:
+                mins[i]=1
+                continue
+            for j in range(i+1,i+nums[i]+1):
+                mins[i]=min(mins[i],1+mins[j])
+        return mins[0]
+                
